3fce01b124c6612c50a4a6634ad675a9d6f019cb
Bayram Enes Atay <bayrameness4242@gmail.com>
2025-07-22 14:58:44 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,27 +1,31 @@
 
-
-
-  const url="https://api.jikan.moe/v4/anime?page=1";
-  fetch(url)
-.then(response=>response.json())
-.then(json=>{
+const url="https://api.jikan.moe/v4/anime?page=1";
+fetch(url)
+  .then(response=>response.json())
+  .then(json=>{
   
-  let data=json.data;
-  const bottomdiv=document.querySelector("#bottomdiv");
+    let data=json.data;
+    const bottomdiv=document.querySelector("#bottomdiv");
+    let pageCount=1;
+    let lastvisiblepage=10;
+    data.forEach(anime => {
 
-  data.forEach(anime => {
-
-      let card=document.createElement("div");
+      let card=document.createElement("article");
       card.id="card";
-      let imagediv=document.createElement("div");
+      let imagediv=document.createElement("figure");
       imagediv.id="imagediv";
       let image=anime.images.jpg.image_url;
-      let description=document.createElement("div");
-      description.id="info"
+      let description=document.createElement("aside");
+      description.id="info";
       let type=anime.type;
       let episodes=anime.episodes;
       let score=anime.score;
+      let detail=document.createElement("sl-details");
+    
+      detail.setAttribute("summary","The Story");
 
+      detail.textContent=anime.synopsis;
+      //detail kısmı
       let trailer=anime.trailer.url;
       description.innerHTML=`
         <a href=${trailer}>Click to see trailer</a>
@@ -29,46 +33,41 @@
         <h5>episodes: ${episodes}</h5>
         <h5>Score: ${score}</h5>
         
-      `
-
+      `;
       imagediv.innerHTML=`
       <img src="${image}" height=270>
-      `
+      `;
       card.innerHTML=`
       <h4>${anime.title}</h4>
       
-     
-
-      `
+      `;
+      description.appendChild(detail);
       imagediv.appendChild(description);
       card.appendChild(imagediv);
 
       bottomdiv.appendChild(card);
-  });
-
-  let btndiv=document.createElement("div");
-    btndiv.id="btndiv";
-  for(let i=0;i<10;i++)
-  {
-    let btn=document.createElement("button");
-    btn.addEventListener("click",function(){
-      page(i+1);
     });
-    btn.innerHTML=i+1;
-    btn.id="btn";
-    
-    btndiv.appendChild(btn);
-    
-    
+    let btndiv=document.createElement("footer");
+    btndiv.id="btndiv";
 
-  }
-  let topofpage=document.createElement("a");
+    for( pageCount;pageCount<=lastvisiblepage;pageCount++)
+    {
+      let counter=pageCount;
+      let btn=document.createElement("button");
+      btn.addEventListener("click",function(){
+        list_theseries(counter);
+      });
+      btn.innerHTML=pageCount;
+      btn.className="btn";
+    
+      btndiv.appendChild(btn);
+
+    }
+    let topofpage=document.createElement("a");
     topofpage.href="#topdiv";
     topofpage.textContent="Top of Page";
     btndiv.appendChild(topofpage);
     topofpage.id="topofpage";
-
-     
 
     bottomdiv.appendChild(btndiv);
     if(!document.querySelector("#bottomofpage"))
@@ -79,100 +78,93 @@
       bottomofpage.textContent="Bottom of Page";
       document.querySelector("#topdiv").appendChild(bottomofpage);
     }
-    
 
-})
+  });
 
-
-function page(i)
+function list_theseries(i)
 {
- 
   const url="https://api.jikan.moe/v4/anime?page="+i;
   fetch(url)
-.then(response=>response.json())
-.then(json=>{
+    .then(response=>response.json())
+    .then(json=>{
   
-  let data=json.data;
-  const bottomdiv=document.querySelector("#bottomdiv");
-  bottomdiv.innerHTML="";
+      let data=json.data;
+      const bottomdiv=document.querySelector("#bottomdiv");
+      bottomdiv.innerHTML="";
+      let pageCount=1;
+      let lastvisiblepage=10;
+  
+      data.forEach(anime => {
 
-  data.forEach(anime => {
+        let card=document.createElement("article");
+        card.id="card";
+        let imagediv=document.createElement("figure");
+        imagediv.id="imagediv";
+        let image=anime.images.jpg.image_url;
+        let description=document.createElement("aside");
+        description.id="info";
+        let type=anime.type;
+        let episodes=anime.episodes;
+        let score=anime.score;
 
-      let card=document.createElement("div");
-      card.id="card";
-      let imagediv=document.createElement("div");
-      imagediv.id="imagediv";
-      let image=anime.images.jpg.image_url;
-      let description=document.createElement("div");
-      description.id="info"
-      let type=anime.type;
-      let episodes=anime.episodes;
-      let score=anime.score;
-
-      let trailer=anime.trailer.url;
-      description.innerHTML=`
+        let trailer=anime.trailer.url;
+        description.innerHTML=`
         <a href=${trailer}>Click to see trailer</a>
         <h5>Type: ${type}</h5>
         <h5>episodes: ${episodes}</h5>
         <h5>Score: ${score}</h5>
         
-      `
+      `;
 
-      imagediv.innerHTML=`
+        imagediv.innerHTML=`
       <img src="${image}" height=270>
-      `
-      card.innerHTML=`
+      `;
+        card.innerHTML=`
       <h4>${anime.title}</h4>
       
      
 
-      `
-      imagediv.appendChild(description);
-      card.appendChild(imagediv);
+      `;
+        imagediv.appendChild(description);
+        card.appendChild(imagediv);
 
-      bottomdiv.appendChild(card);
-  });
+        bottomdiv.appendChild(card);
+      });
+      let btndiv=document.createElement("footer");
+      btndiv.id="btndiv";
 
-  let btndiv=document.createElement("div");
-    btndiv.id="btndiv";
-
-  for(let i=0;i<10;i++)
-  {
-    let btn=document.createElement("button");
-    btn.addEventListener("click",function(){
-      page(i+1);
+      for(pageCount;pageCount<=lastvisiblepage;pageCount++)
+      {
+        let counter=pageCount;
+        let btn=document.createElement("button");
+        btn.addEventListener("click",function(){
+          list_theseries(counter);//when ı write here pageCount then the varıable have the last pageCount value at the end.
       
-    });
-    btn.innerHTML=i+1;
-    btn.id="btn";
+        });
+        btn.innerHTML=pageCount;
+        btn.className="btn";
     
-    btndiv.appendChild(btn);
+        btndiv.appendChild(btn);
     
-    
-  }
+      }
   
-let topofpage=document.createElement("a");
-    topofpage.href="#topdiv";
-    topofpage.id="topofpage";
-    topofpage.textContent="Top of Page";
-    btndiv.appendChild(topofpage);
+      let topofpage=document.createElement("a");
+      topofpage.href="#topdiv";
+      topofpage.id="topofpage";
+      topofpage.textContent="Top of Page";
+      btndiv.appendChild(topofpage);
     
-    bottomdiv.appendChild(btndiv);
+      bottomdiv.appendChild(btndiv);
    
-    if(!document.querySelector("#bottomofpage"))
-    {
-      let bottomofpage=document.createElement("a");
-      bottomofpage.id="bottomofpage";
-      bottomofpage.href="#btndiv";
-      bottomofpage.textContent="Bottom of Page";
-      document.querySelector("#topdiv").appendChild(bottomofpage);
-    }
-   
+      if(!document.querySelector("#bottomofpage"))
+      {
+        let bottomofpage=document.createElement("a");
+        bottomofpage.id="bottomofpage";
+        bottomofpage.href="#btndiv";
+        bottomofpage.textContent="Bottom of Page";
+        document.querySelector("#topdiv").appendChild(bottomofpage);
+      }
     
-
-    
-})
+    });
 }//load the rest
 //and the descriptions
-
-
