6cb914bd141c614693a8f63060a42d63107aa8c0
Bayram Enes Atay <bayrameness4242@gmail.com>
2025-07-23 11:29:35 +0300

    Update moreinfo.js

--- moreinfo.js
+++ moreinfo.js
@@ -1,9 +1,10 @@
-const parametres=new URLSearchParams(window.location.search);// helps us work with the url parametres.(with functions)
+const parametres=new URLSearchParams(window.location.search);
+// helps us work with the url parametres.(with functions)
 const id=parametres.get("id");
 const url="https://api.jikan.moe/v4/anime/"+id;
 fetch(url)
-  .then(Response=>Response.json())
-  .then(json=>{
+  .then(Response => Response.json())
+  .then(json => {
     let data=json.data;
     let title=data.title;
     let alttitle=data.title_japanese;
